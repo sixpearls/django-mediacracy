0d64ca808ac927afe8cac28671127b79fda2bef3
Ben Margolis <ben@sixpearls.com>
2013-02-18 23:42:27 -0800

    injecting extra image fields to massmedia.Image and automatically handling generation and deletion

--- mediacracy/models.py
+++ mediacracy/models.py
@@ -1,10 +1,130 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.template.loader import get_template#, select_template
 from django.conf import settings as site_settings
+from django.db.models.signals import class_prepared, pre_delete
 from django.utils.translation import ugettext, ugettext_lazy as _
 
 from mediacracy import settings
 
 from django import template
 template.add_to_builtins('mediacracy.templatetags.mediacracy_tags')
+
+def add_image_fields(sender, **kwargs):
+    """
+    class_prepared signal handler that checks for the model massmedia.Image
+    and adds sized image fields
+    """
+    if sender.__name__ == "Image" and sender._meta.app_label == 'massmedia':
+        large = models.ImageField(upload_to=".", blank=True, verbose_name=_('large image file')) #1024
+        medium = models.ImageField(upload_to=".", blank=True, verbose_name=_('medium image file')) #600
+        small = models.ImageField(upload_to=".", blank=True, verbose_name=_('small image file')) #300
+        
+        large.contribute_to_class(sender, "large")
+        medium.contribute_to_class(sender, "medium")
+        small.contribute_to_class(sender, "small")
+
+class_prepared.connect(add_image_fields)
+
+from massmedia.models import Collection, Image
+from PIL import Image as PILImage
+from cStringIO import StringIO
+from django.core.files.uploadedfile import SimpleUploadedFile
+import os
+from massmedia.settings import IMAGE_UPLOAD_TO
+from time import strftime
+
+def collection_render_detail(self):
+    tmpl = get_template('massmedia/mediatypes/collection_detail.html')
+    ctxt = template.Context({
+        'object': self,
+    })
+    return tmpl.render(ctxt)
+Collection.render_detail = collection_render_detail
+
+Image.image_fields = [
+    Image._meta.get_field_by_name('file')[0],
+    Image._meta.get_field_by_name('large')[0],
+    Image._meta.get_field_by_name('medium')[0],
+    Image._meta.get_field_by_name('small')[0],
+    Image._meta.get_field_by_name('thumbnail')[0],]
+
+def resize(file_name, original, size):
+    #code taken from http://djangosnippets.org/snippets/2094/
+    #accessed on 7/22/2011
+    thisImage = original.copy()
+    thisImage.thumbnail( (size,size), PILImage.ANTIALIAS)
+    #save the thumbnail to memory
+    temp_handle = StringIO()
+    thisImage.save(temp_handle, 'png')
+    temp_handle.seek(0)
+    #save the thumbnail field
+    thumbname_tuple = os.path.split(file_name)[-1].split('.')
+    thumbname = thumbname_tuple[0] + '_' + str(size) + '.png'
+    suf = SimpleUploadedFile(thumbname, temp_handle.read(), content_type='image/png')
+    return suf
+
+def image_generate_thumbnails(self):
+    if self.external_url:
+        import urllib
+        from urlparse import urlparse
+        filepath, headers = urllib.urlretrieve(self.external_url)
+        image = PILImage.open(filepath)
+        filename = os.path.basename(urlparse(self.external_url).path)
+    elif self.file:
+        image = PILImage.open(self.file.path)
+        filename = os.path.basename(self.file.name)
+    if image.mode not in ('L', 'RGB'):
+        image = image.convert('RGB')
+
+    try:
+        this_path = os.path.split(self.file.name)[0]+'/'
+    except:
+        this_path = strftime(IMAGE_UPLOAD_TO)+'/'
+    original_width, original_height = image.size
+    
+    for (field_name,size) in settings.IMAGE_SIZES.items():
+        image_field = getattr(self,field_name,None)
+        if image_field is not None:
+            if (original_width > size or original_height > size): 
+                suf = resize(filename, image, size)
+                preferredname = this_path+suf.name
+                if (image_field.storage.exists(preferredname)):
+                    image_field.storage.delete(preferredname)
+                newname = image_field.storage.get_available_name(preferredname)
+                image_field.storage.save(name=newname,content=suf)#,save=False)
+                image_field.name = newname #okay
+            else:
+                image_field.name = self.file.name
+
+def image_delete_files(self):
+    for field in self.image_fields:
+        field_ref = getattr(self,field.name)
+        try:
+            field_ref.delete(save=False)
+        except:
+            pass
+
+def image_save(self):
+    do_generate_thumbs = False
+    if self.pk is not None:
+        this = type(self).objects.get(pk=self.pk)
+        if os.path.basename(self.file.name) != os.path.basename(this.file.name):
+            this._delete_files()
+            do_generate_thumbs = True
+    else:
+        do_generate_thumbs = True
+    super(Image, self).save()
+    if do_generate_thumbs:
+        self._generate_thumbnails()
+        super(Image, self).save()
+
+Image._generate_thumbnails = image_generate_thumbnails
+Image.save = image_save
+Image._delete_files = image_delete_files
+
+def delete_image_files(sender,instance,*args,**kwargs):
+    if sender.__name__ == "Image" and sender._meta.app_label == 'massmedia':
+        instance._delete_files()
+pre_delete.connect(delete_image_files)
--- mediacracy/settings.py
+++ mediacracy/settings.py
@@ -2,9 +2,15 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 
+DEFAULT_IMAGE_SIZES = {
+    'large': 1024,
+    'medium': 600,
+    'small': 300,
+    'thumbnail': 150,
+}
 
 DEFAULT_SETTINGS = {
-
+    'IMAGE_SIZES': DEFAULT_IMAGE_SIZES,
 }
 
 USER_SETTINGS = DEFAULT_SETTINGS.copy()
